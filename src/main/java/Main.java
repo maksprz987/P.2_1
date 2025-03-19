9368bc029cf4a9739bd26d5f3366787688920e76
maksprz987 <maszywa2810@gmail.com>
2025-03-19 13:36:14 +0000

    koniec

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,16 @@
 // import static org.junit.jupiter.api.Assertions.assertEquals;
 
 // import org.junit.jupiter.api.Test;
+import java.util.Scanner;
 
 public class Main {
   public static void main(String[] args) {
-    String text = "hello world!";
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("Pisz: ");
+    String text = scanner.nextLine();
     String upperCase = text.toUpperCase();
     System.out.println(upperCase);
-    
+    scanner.close();
   }
 }
 
